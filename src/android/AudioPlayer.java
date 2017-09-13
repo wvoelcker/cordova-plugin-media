fe941eb8bc0101ef079737256767cf933baadefd
Will Voelcker <wvoelcker@yahoo.co.uk>
2017-09-13 16:10:06 +0100

    Remove debug

--- src/android/AudioPlayer.java
+++ src/android/AudioPlayer.java
@@ -261,11 +261,9 @@ public class AudioPlayer implements OnCompletionListener, OnPreparedListener, On
 
         /* Diversion from main repo */
         if (!stop) {
-            LOG.d(LOG_TAG, "stop was false");
             this.pauseRecording();
             return;
         } else {
-            LOG.d(LOG_TAG, "stop was true");
 
             // Resume if paused to prevent crash
             this.resumeRecording();
